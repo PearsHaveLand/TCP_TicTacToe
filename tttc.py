414988489021d45b076f686b21d42e2eeb06832f
Pearce Haviland <pearce.haviland@protonmail.com>
2021-04-03 12:55:50 -0400

    Re-wrote basic functionality
    Shoot, I realized this was an older version than I thought it was. So I fixed it up to have basic tic-tac-toe functionality.

--- tttc.py
+++ tttc.py
@@ -18,91 +18,122 @@ CLIENT_FIRST_STR = "01000000"
 CLIENT_FIRST = int(CLIENT_FIRST_STR, 2)
 EMPTY_MSG_STR = "00000000"
 EMPTY_MSG = int(EMPTY_MSG_STR, 2)
+COORDS_SECTION = int("00001111", 2)
+BOARD_SPOTS = [' ', ' ',' ', ' ',' ', ' ',' ', ' ',' ']
 
 #Returns numerical representation of binary string str_dec
 def get_bin(str_dec):
-	return int(str, 2)
+    return int(str, 2)
+
+def get_coords(byte_msg):
+    return byte_msg & COORDS_SECTION
 
 #Uses a provided string to generate a message to send to the server
 #Populates a byte of information
 def generate_message(str):
-	if str == "exit":
-		return EXIT_BYTE
-	#Since this will only be called after validating input, there is
-	#no need to further validate input
-	else:
-		return int(str)
-	
+    if str == "exit":
+        return EXIT_BYTE
+    #Since this will only be called after validating input, there is
+    #no need to further validate input
+    else:
+        return int(str)
+    
 #Ensures that the user's input meets the standards of the protocol
 def validate_input(str):
-	#First, check if exiting
-	if str == "exit":
-		return True
-		
-	#Next, check if numerical coordinates are correct
-	elif str.isdigit() and len(str) == 1:
-	
-		#If both numbers are between 0 and 2, inclusive
-		if int(str) <= 8 and int(str) >= 0:
-			return True
-	
-	#If none of these conditions are met, return false
-	return False
+    #First, check if exiting
+    if str == "exit":
+        return True
+        
+    #Next, check if numerical coordinates are correct
+    elif str.isdigit() and len(str) == 1:
+    
+        #If both numbers are between 0 and 2, inclusive
+        if int(str) <= 8 and int(str) >= 0:
+            return True
+    
+    #If none of these conditions are met, return false
+    return False
 
 #function to get user input
 #returns a string indicating the coordinates selected by the user
 def get_input():
-	valid_input = False
-	user_input = ''
-	#Until valid input is entered, continue prompting user
-	while not valid_input:
-		print("Enter a position (0 to 8), or 'exit' to quit.")
-		user_input = str(input("Coordinates: "))
-		valid_input = validate_input(user_input)
-		
-	return generate_message(user_input)
-	
+    valid_input = False
+    user_input = ''
+    #Until valid input is entered, continue prompting user
+    while not valid_input:
+        print("Enter a position (0 to 8), or 'exit' to quit.")
+        user_input = str(input("Coordinates: "))
+        valid_input = validate_input(user_input)
+        
+    # Handle exit
+    if user_input != "exit":
+        BOARD_SPOTS[int(user_input)] = 'X'
+
+    return generate_message(user_input)
+    
 def send_input(socket, msg):
-	msg_arr = bytearray()
-	msg_arr.append(msg)
-	socket.send(msg_arr)
-	
+    msg_arr = bytearray()
+    msg_arr.append(msg)
+    socket.send(msg_arr)
+    
 def recv_msg(socket):
-	return socket.recv(1024)[0]
-	
+    return socket.recv(1024)[0]
+    
+def make_move_s(position):
+    BOARD_SPOTS[position] = 'O'
+    return "FULL"
+
 def init_game(socket, client_first):
-	msg = 0
-	if(client_first):
-		msg = CLIENT_FIRST
-	else:
-		msg = EMPTY_MSG
-	send_input(socket, msg)
-	
-def run_game(socket, me_first):
-	msg = bytearray()
-	
-	#Whether or not the player wishes to exit the game
-	game_exit = False
-	
-	init_game(socket, me_first)
-	
-	#While the player continues to play the game, do not exit
-	while not game_exit:
-		msg = get_input()
-		if(msg == EXIT_BYTE):
-			game_exit = True
-		send_input(socket, msg)	
-	return
-	
+    msg = 0
+    if(client_first):
+        msg = CLIENT_FIRST
+    else:
+        msg = EMPTY_MSG
+    send_input(socket, msg)
+    
+def run_game(socket, me_first=False):
+    msg = bytearray()
+    
+    #Whether or not the player wishes to exit the game
+    game_exit = False
+    
+    init_game(socket, me_first)
+    
+    #While the player continues to play the game, do not exit
+    while not game_exit:
+
+        # Wait for server move
+        msg = recv_msg(socket)
+        print("server message:", msg)
+        make_move_s(get_coords(msg))
+
+        render_board()
+
+        msg = get_input()
+        if(msg == EXIT_BYTE):
+            game_exit = True
+        send_input(socket, msg)	
+    return
+    
+def render_board():
+    display = ""
+    for index in range(len(BOARD_SPOTS)):
+        if index % 3 == 0:
+            display += '\n' + BOARD_SPOTS[index]
+        else:
+            display += '|' + BOARD_SPOTS[index]
+    print(display)
+    
+
 def main():
-	server_name = input("Enter the IP address of the tic-tac-toe server: ")
-	server_port = 13037
-	client_socket = socket(AF_INET, SOCK_STREAM)
-	client_socket.connect((server_name, server_port))
-	run_game(client_socket)
-	client_socket.close()
-	#coords = get_input()
-	#client_socket.send(coords.encode())
-	
+    server_name = input("Enter the IP address of the tic-tac-toe server: ")
+    server_port = 13037
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((server_name, server_port))
+    run_game(client_socket)
+    client_socket.close()
+    #coords = get_input()
+    #client_socket.send(coords.encode())
+    
 if __name__ == "__main__":
-	main()
+    main()
--- ttts.py
+++ ttts.py
@@ -15,76 +15,91 @@ CLIENT_MARK = 'X'
 SERVER_MARK = 'O'
 
 def send_msg(socket, msg):
-	msg_arr = bytearray()
-	msg_arr.append(msg)
-	socket.send(msg_arr)
+    msg_arr = bytearray()
+    msg_arr.append(msg)
+    socket.send(msg_arr)
 
 def get_coords(byte_msg):
-	return byte_msg & COORDS_SECTION
+    return byte_msg & COORDS_SECTION
 
 def make_move_s(board):
-	for space in board:
-		if space == EMPTY_SPACE:
-			space = SERVER_MARK
-			return board
-	return "FULL"
+    for index in range(len(board)):
+        if board[index] == EMPTY_SPACE:
+            board[index] = SERVER_MARK
+            return board, index
+    return "FULL", -1
 
 def make_move_c(board, coord):
-	if board[coord] == EMPTY_SPACE:
-		board[coord] = CLIENT_MARK
-	return board
+    if board[coord] == EMPTY_SPACE:
+        board[coord] = CLIENT_MARK
+    return board
 
 def check_client_first(socket, board):
-	init_msg = socket.recv(1024)[0]
-	coord = 0
-	
-	#If init_msg contains CLIENT_FIRST indicator
-	if init_msg & CLIENT_FIRST == CLIENT_FIRST:
-		
-		#Get client coordinate, then apply their move choice
-		coord = get_coords(init_msg)
-		make_move_c(board, coord)
-	
-	return board
-	
+    init_msg = socket.recv(1024)[0]
+    coord = 0
+    
+    #If init_msg contains CLIENT_FIRST indicator
+    if init_msg & CLIENT_FIRST == CLIENT_FIRST:
+        
+        #Get client coordinate, then apply their move choice
+        coord = get_coords(init_msg)
+        make_move_c(board, coord)
+    
+    return board
+    
 #Creates and runs new game
 #	Game has different port number than original server
 #	Communicates with client to play
 def run_game(socket, port, addr):
-	client_msg = 0
-	game_positions = ""
-	client_first = False
-	
-	#Populate board with empty spaces
-	for i in range(0, BOARD_SIZE):
-		game_positions += EMPTY_SPACE
-		
-	check_client_first(socket, board)
-	
-	#Keep running until the user exits
-	while not (client_msg == EXIT_BYTE):
-		
-		#Receive the message
-		client_msg = socket.recv(1024)[0]
-		
-		#If the message is NOT the exit code
-		if not client_msg == EXIT_BYTE:
-			send_msg(socket, client_msg)
-		
-	socket.close()
-	print("connection closed")
-	
+    client_msg = 0
+    game_positions = []
+    client_first = False
+    
+    #Populate board with empty spaces
+    for i in range(0, BOARD_SIZE):
+        game_positions.append(EMPTY_SPACE)
+        
+    # Check if the client is first, make moves accordingly
+    check_client_first(socket, game_positions)
+    game_positions, pos = make_move_s(game_positions)
+    render_board(game_positions)
+    send_msg(socket, pos)
+    
+    #Keep running until the user exits
+    while not (client_msg == EXIT_BYTE):
+        #Receive the message
+        client_msg = socket.recv(1024)[0]
+        
+        #If the message is NOT the exit code
+        if client_msg != EXIT_BYTE:
+            game_positions = make_move_c(game_positions, get_coords(client_msg))
+            game_positions, pos = make_move_s(game_positions)
+            render_board(game_positions)
+            send_msg(socket, pos)
+        
+    socket.close()
+    print("connection closed")
+    
+def render_board(board):
+    display = ""
+    for index in range(len(board)):
+        if index % 3 == 0:
+            display += '\n' + board[index]
+        else:
+            display += '|' + board[index]
+    print(display)
+
 #Listens for clients, creates new game instances for each client
 def main():
-	server_port = 13037
-	server_socket = socket(AF_INET, SOCK_STREAM)
-	server_socket.bind(('',server_port))
-	server_socket.listen(1)
-	print('The server is ready to receive')
-	while True:
-		#addr is a tuple, containing address and port number
-		connection_socket, (addr, client_port) = server_socket.accept()
-		_thread.start_new_thread(run_game, (connection_socket, client_port, addr))
-		
+    server_port = 13037
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind(('',server_port))
+    server_socket.listen(1)
+    print('The server is ready to receive')
+    while True:
+        #addr is a tuple, containing address and port number
+        connection_socket, (addr, client_port) = server_socket.accept()
+        _thread.start_new_thread(run_game, (connection_socket, client_port, addr))
+        
 if __name__ == "__main__":
-	main()
+    main()
